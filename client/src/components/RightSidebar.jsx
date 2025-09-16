650f3a8c1be498a940d250fd75825296874d45a7
AmirAliAmin <aliaminamir77@gmail.com>
2025-09-16 15:06:17 +0500

    completed

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import assets, { imagesDummyData } from '../assets/assets'
+import { ChatContext } from '../../context/ChatContext'
+import { AuthContext } from '../../context/AuthContext';
 
-export default function RightSidebar({selectedUser, setSelectedUser}) {
+export default function RightSidebar() {
+  const {selectedUser, message} = useContext(ChatContext);
+  const {logout, onlineUsers} = useContext(AuthContext);
+  const [msgImg, setMsgImg] = useState([]);
+
+  //get all the img from the message and set them to state
+  useEffect(() => {
+    setMsgImg(
+      message.filter(msg=>msg.image).map(msg=>msg.image)
+    )
+    
+  }, [message])
+
+
   return selectedUser && (
     <div className={`bg-[#8185B2]/10 text-white w-full relative overflow-y-scroll ${selectedUser ? "max-md:hidden":" "}`}>
      <div className='pt-5 flex flex-col items-center gap-2 text-xs font-light mx-auto'>
       <img src={selectedUser?.profilePic || assets.avatar_icon} alt=""  className='w-20 aspect-[1/1 rounded-full'/>
       <h1 className='px-5 text-xl font-medium mx-auto flex items-center gap-2'>
-        <p className='w-2 h-2 rounded-full bg-green-500'></p>
+        {onlineUsers.includes(selectedUser._id) && (
+          <p className='w-2 h-2 rounded-full bg-green-500'></p>
+        )}
         {selectedUser.fullName}</h1>
        <p className='px-5 mx-auto'>{selectedUser.bio}</p>
      </div>
@@ -16,7 +33,7 @@ export default function RightSidebar({selectedUser, setSelectedUser}) {
       <p>Media</p>
       <div className='mt-2 mb-10 max-h-[200px] overflow-y-scroll grid grid-cols-2 gap-4 opacity-80'>
         {
-          imagesDummyData.map((url, index)=>(
+          msgImg.map((url, index)=>(
             <div key={index} onClick={()=>window.open(url)} className='cursor-pointer rounded'>
               <img src={url} alt="" className='h-full rounded-md' />
             </div>
@@ -24,7 +41,7 @@ export default function RightSidebar({selectedUser, setSelectedUser}) {
         }
       </div>
      </div>
-     <button className='absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-gradient-to-r from-purple-400 to-violet-600 text-white border-none text-sm font-light py-2 px-20 rounded-full cursor-pointer'>
+     <button onClick={()=>logout()} className='absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-gradient-to-r from-purple-400 to-violet-600 text-white border-none text-sm font-light py-2 px-20 rounded-full cursor-pointer'>
       Logout
      </button>
 
